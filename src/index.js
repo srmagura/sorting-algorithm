b1e3718a0fe3fc899a8f8f4bdfcfb4e0c72a409d
Sam Magura <srmagura@gmail.com>
2022-10-06 18:31:04 +0000

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import "./styles.css";
+
+import { original } from "./original";
+
+document.getElementById("original").innerText = original.join(", ");
+
+document.getElementById("sorted").innerText = original.join(", ");
